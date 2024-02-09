e50767bc7718424b0c3b5c9bb55b946e202b204f
devmahin <mahinhowlader123@gmail.com>
2024-02-09 22:45:38 +0600

    dom-style

---
+++ script.js
@@ -0,0 +1,11 @@
+// const li = document.getElementsByTagName("li")
+// for(let i =0; i < li.length; i++){
+//     console.log(li[i].innerText)
+// }
+// const h1 = document.getElementsByTagName("h2")
+// for (let bang of h1){
+//     console.log(bang.innerText)
+// }
+
+let news = document.getElementById("new");
+console.log(news.classList)
