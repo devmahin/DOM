a8ea1992f83883ffe69863d28e92449285a3f701
devmahin <mahinhowlader123@gmail.com>
2024-02-09 23:04:48 +0600

    attribute

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const section = document.getElementsByTagName("section");
 const section2 = document.getElementsByTagName("section")[1];
-console.log(section2)
+    let attSet = section2.setAttribute("class", "section2")
+    let att = section2.getAttribute("class")
+
+    console.log(att)
 
 for(let sec of section){
-    console.log(sec)
+    // console.log(sec)
     sec.style.border = "2px solid red";
     sec.style.padding = "20px";
     sec.style.margin = "20px";
